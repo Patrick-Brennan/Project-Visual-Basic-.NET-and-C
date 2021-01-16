b0c1c74e019eedf90cd75532b4b28cf24eaca7cf
Patrick-Brennan <patrickb2@hotmail.ca>
2021-01-15 21:54:40 -0500

    Changed to WPF App (.NET Framework), changed aligning, made content an attribute.

---
+++ CurrencyConverter/CurrencyConverter/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurrencyConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            // Button to convert currency.
+            CurrencyConvert();
+        }
+
+        private void txtAmount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Enter currency amount to be converted.
+            CurrencyConvert();
+        }
+
+        private void CurrencyConvert()
+        {
+            double currency;
+            if (!double.TryParse(txtAmount.Text, out currency))
+            {
+                return;
+            }
+        }
+    }
+}
