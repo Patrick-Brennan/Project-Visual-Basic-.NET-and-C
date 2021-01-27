bdd19e174615d474954307075370241942cb824f
Patrick-Brennan <patrickb2@hotmail.ca>
2021-01-26 22:56:28 -0500

    Just taking a dabble at database myself. Only just added the three c# classes, and nothing else.

---
+++ CurrencyConverter/CurrencyConverter/Conversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    class Conversion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string lbResult { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string lbDate { get; set; }
+    }
+}
---
+++ CurrencyConverter/CurrencyConverter/CurrencyConverterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    class CurrencyConverterDbContext : DbContext
+    {
+        const string DbName = "[Insert Database Name]";
+        static string DbPath = Path.Combine(Environment.CurrentDirectory, DbName);
+        public CurrencyConverterDbContext() : base($@"Data Source=[Insert Database Source]")
+        { }
+        public virtual DbSet<Conversion> Conversion { get; set; }
+    }
+}
